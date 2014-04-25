0ef3dd7afa6f908b866517fa5cb6332508aa9477
amirboldo <265eedad@opayq.com>
2014-04-25 21:59:46 +0300

    my solution for peer-assignment. implementation of makeCacheMatrix & cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This is the implementation of the peer-assignment of R Programming course
+## Contrains 2 functions- first, makeCacheMatrix to create a "special" matrix 
+## to be used to solve a matrix with caching the result. Second function is 
+## cacheSolve used to solve a "special" matrix.
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## This function creates a special "matrix" object that can cache its inverse.
+makeCacheMatrix <- function(x = matrix()) {	
+        r <- NULL
+        set <- function(y) {
+                x <<- y
+                r <<- NULL
+        }
+        get <- function() x
+        setreverse <- function(reversed) r <<- reversed
+        getreverse <- function() r
+        list(set = set, get = get,
+             setreverse = setreverse,
+             getreverse = getreverse)
 }
 
 
-## Write a short comment describing this function
-
+## This function computes the inverse of the special "matrix" 
+## returned by makeCacheMatrix above
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+	  r <- x$getreverse()
+        if(!is.null(r)) {
+                message("getting cached data")
+                return(r)
+        }
+        data <- x$get()
+	  r <- solve(data, ...)
+        x$setreverse(r)
+        r
 }
+
